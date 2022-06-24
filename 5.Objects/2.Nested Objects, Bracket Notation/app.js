6f9d27062e17a9be8b1a8fa4579b109bc0b665d7
Hubert Kulicki <hubertkulicki@gmail.com>
2022-06-24 09:00:17 +0200

    quicksave

---
+++ 5.Objects/2.Nested Objects, Bracket Notation/app.js
@@ -0,0 +1,16 @@
+// nested objects
+// set variable as property value
+// dot notation vs bracket notation
+
+const person = {
+    name:'john',
+    age: 25,
+    married: true,
+    siblings:['anna', 'peter'],
+    greet:function(name){
+        console.log(`Hello, my name is ${name}`);
+    },
+    sayHello(name){
+        console.log(`Hello, my name is ${name}`);
+    }
+}
