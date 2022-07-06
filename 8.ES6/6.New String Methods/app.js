e30b71c2eb03206b8685f21b7b2548bf15d806a8
Hubert Kulicki <hubertkulicki@gmail.com>
2022-07-06 15:11:00 +0200

    added string.includes()

--- 8.ES6/6.New String Methods/app.js
+++ 8.ES6/6.New String Methods/app.js
@@ -1,26 +1,53 @@
 // new string methods
 // startsWith(), endsWith(), includes(), repeat()
 
-const person = 'Peter Smith';
+const person = "Peter Smith";
 const employee = `23456-EMP-PETER-SMITH`;
-const manager = "23456-MAN-JOHN-DOE"
+const manager = "23456-MAN-JOHN-DOE";
 
 // startsWith()
-console.log(person.startsWith('Pete')); //case sensitive
-console.log(person.startsWith('peter')); //case sensitive
-console.log(employee.startsWith('EMP', 6));
+console.log(person.startsWith("Pete")); //case sensitive
+console.log(person.startsWith("peter")); //case sensitive
+console.log(employee.startsWith("EMP", 6));
 
 // endsWith()
-console.log(manager.endsWith('DOE'));
-console.log(manager.endsWith('MAN', 9));
+console.log(manager.endsWith("DOE"));
+console.log(manager.endsWith("MAN", 9));
 
 // includes
-console.log(employee.includes('PETER'));
-console.log(employee.includes('pETER'));
-
+console.log(employee.includes("PETER"));
+console.log(employee.includes("pETER"));
 
 // repeat
-const  multiplyPeople = (person, amount=5) => person.repeat(amount);
+const multiplyPeople = (person, amount = 5) => person.repeat(amount);
 
 const people = multiplyPeople(person, 10);
 console.log(people);
+
+// String includes() - checks if a string contains another string.
+
+const products = [
+  { title: "Modern Poster" },
+  { title: "Bar Stool" },
+  { title: "Armchair" },
+  { title: "Leather Chair" },
+];
+
+const text = "a";
+
+const filteredProducts = products.filter((product) =>
+  product.title.toLowerCase().includes(text)
+);
+
+console.log(filteredProducts);
+
+// const firstName = "john";
+// const result = firstName.includes("Jo", 1);
+// console.log(result);
+
+const product = {
+  title: "Leather Chair",
+};
+
+const result = product.title.includes("Le");
+console.log(result);
