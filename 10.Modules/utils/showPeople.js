4e069d973e7542d744bf2a3cf83e223539b1a150
Hubert Kulicki <hubertkulicki@gmail.com>
2022-07-07 15:12:12 +0200

    upload

---
+++ 10.Modules/utils/showPeople.js
@@ -0,0 +1,11 @@
+const showPeople = (people) => {
+  const newPeople = people
+    .map((person) => {
+      const { name, job } = person;
+      return `<p>${name} <strong>${job}</strong></p>`;
+    })
+    .join('');
+  return newPeople;
+};
+
+export default showPeople;
